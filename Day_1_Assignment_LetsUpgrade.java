73dc983e54c579600e4563d6a8d4dda52763d720
AkanshGwal01 <91788769+AkanshGwal01@users.noreply.github.com>
2021-10-02 19:02:49 +0530

    Day 1 Assignment uploaded.

---
+++ Day_1_Assignment_LetsUpgrade.java
@@ -0,0 +1,9 @@
+
+public class Day_1_Assignment_LetsUpgrade {
+
+	public static void main(String[] args) {
+		System.out.println("Hi! My name is Akansh Gwalwanshi.\nI am currently pursuing my B.Tech in Computer Science and Engineering.\nI have a huge interest in computer programming,\nI learned C++ in my Higher Secondary School.\nI am also a trainee web developer.");
+
+	}
+
+}
